c6b002057057e6f417d916d9604dc38418e2d48b
Ben Noordhuis <info@bnoordhuis.nl>
2012-06-18 16:15:58 +0200

    test: beef up test

--- test/test-dgram-unix.js
+++ test/test-dgram-unix.js
@@ -1,18 +1,34 @@
-SOCKNAME = '/tmp/unix_dgram.sock';
+var assert = require('assert');
+var fs = require('fs');
 
 var unix = require('../src/unix_dgram');
+var SOCKNAME = '/tmp/unix_dgram.sock';
+
+var sentPing = false;
+var seenPing = false;
+
+process.on('exit', function() {
+  assert.equal(sentPing, true);
+  assert.equal(seenPing, true);
+});
+
+try { fs.unlinkSync(SOCKNAME); } catch (e) { /* swallow */ }
 
 var server = unix.createSocket('unix_dgram', function(buf, rinfo) {
   console.error('server recv', arguments);
+  assert.equal('' + buf, 'PING');
+  seenPing = true;
   server.close();
   client.close();
-  console.error('closed', server, client);
 });
 server.bind(SOCKNAME);
 
 var client = unix.createSocket('unix_dgram', function(buf, rinfo) {
   console.error('client recv', arguments);
+  assert(0);
 });
+
 client.send(Buffer('PING'), 0, 4, SOCKNAME, function() {
   console.error('client send', arguments);
+  sentPing = true;
 });
