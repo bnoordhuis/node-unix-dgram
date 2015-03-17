93aa6a5397c950e6b779b0134d2564e8778c8376
Ben Noordhuis <info@bnoordhuis.nl>
2015-03-17 13:23:19 +0100

    src: upgrade to nan@1.7.0
    Fixes build breakage with io.js.

--- src/unix_dgram.cc
+++ src/unix_dgram.cc
@@ -116,7 +116,7 @@ void OnRecv(SocketContext* sc) {
     }
   }
 
-  argv[0] = NanNew<Integer>(err);
+  argv[0] = NanNew<Integer>(static_cast<int32_t>(err));
 
   TryCatch tc;
   NanNew(sc->recv_cb_)->Call(NanGetCurrentContext()->Global(),
