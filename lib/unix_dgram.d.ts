34e995ab3d5c7e6644ace9bbddb96d16440d39ce
Flaviu Tamas <me@flaviutamas.com>
2025-10-19 17:06:01 -0400

    feat: add typescript support

---
+++ lib/unix_dgram.d.ts
@@ -0,0 +1,47 @@
+/// <reference types="node" />
+
+import { EventEmitter } from "events";
+import { AddressInfo } from "net";
+import TypedEmitter from "typed-emitter";
+
+export function createSocket(
+  type: "unix_dgram",
+  listener?: (msg: Buffer, rinfo: RemoteInfo) => void,
+): Socket;
+
+export interface RemoteInfo {
+  size: number;
+  address: AddressInfo | {};
+  path: string;
+}
+
+export type SocketEvents = {
+  close: () => void;
+  connect: () => void;
+  error: (err: Error) => void;
+  listening: () => void;
+  message: (msg: Buffer, rinfo: RemoteInfo) => void;
+};
+
+export class Socket
+  extends (EventEmitter as new () => TypedEmitter<SocketEvents>) {
+  bind(path: string): void;
+  connect(path: string): void;
+  send(
+    buf: Buffer,
+    offset: number,
+    length: number,
+    path: string,
+    callback?: (err: Error | null) => void,
+  ): void;
+  send(buf: Buffer, callback?: (err: Error | null) => void): void;
+  sendto(
+    buf: Buffer,
+    offset: number,
+    length: number,
+    path: string,
+    callback?: (err: Error | null) => void,
+  ): void;
+  close(): void;
+}
+
